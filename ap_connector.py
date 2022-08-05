c49f0eeeed9b7b5079a304afc9db7fc28430aa97
Krzysiek Sobieraj <krzysiek@sobieraj.mobi>
2022-08-05 15:54:49 +0200

    final version ov web server

--- ap_connector.py
+++ ap_connector.py
@@ -1,5 +1,8 @@
+import json
 import machine
 import network
+import socket
+from response import response
 
 try:
     from secrets import secrets
@@ -7,7 +10,7 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-def estabilishConnection() -> str:
+def establishConnection() -> str:
     wlan = network.WLAN(network.STA_IF)
 
     if not wlan.isconnected():
@@ -23,7 +26,57 @@ def estabilishConnection() -> str:
         
     return ip
 
+def openSocket(ip: str) -> object:
+    address = (ip, 80)
+    connection = socket.socket()
+    
+    connection.bind(address)
+    connection.listen(1)
+    
+    print('listening on', address)
+    
+    return connection
+
+def createResponse() -> str:
+    return str(response)
+
+def serveData(connection: object):
+    dataMock = {
+        "temperature": 34,
+        "humidity": "55%"
+    }
+    
+    responseTemplate = createResponse(dataMock)
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        
+        client.send('HTTP/1.0 200 OK\r\nContent-type: application/json\r\n\r\n')
+        client.send(str(responseTemplate))                
+        client.close()
+
+def createResponse(dataMock: dict) -> dict:
+    try:
+        templateFile = open("response.json", "r")
+    except:
+        print("Response template is kept in response.json file, please add them there!")
+        raise
+        
+    responseTemplate = json.load(templateFile)
+    templateFile.close()
+    
+    for k, v in responseTemplate.items():
+        if k in dataMock:
+            responseTemplate.update({k:dataMock[k]})
+    
+    return responseTemplate
+
 try:
-    ip = estabilishConnection()
+    ip = establishConnection()
+    connection = openSocket(ip)
+    serveData(connection)
 except KeyboardInterrupt:
     machine.reset()
